bf0b985c31a1232bac30d00b78f8b818efa05c92
Maximilian Lohr <s86327@stud.htw-dresden.de>
2025-02-04 15:41:46 +0100

    fixed copyright year

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -280,7 +280,7 @@ const Index = () => {
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="flex flex-col md:flex-row justify-between items-center text-sm text-gray-600">
               <div>
-                © ${currentYear} MaxCyclesCoaching. Alle Rechte vorbehalten.
+                © {currentYear} MaxCyclesCoaching. Alle Rechte vorbehalten.
               </div>
               <div className="mt-4 md:mt-0 space-x-4">
                 <a href="#" className="hover:text-gray-900">Impressum</a>
