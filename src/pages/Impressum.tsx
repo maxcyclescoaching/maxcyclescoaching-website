cb83d86cc250f184724bccd6c92d6c7a698351f6
Maximilian Lohr <s86327@stud.htw-dresden.de>
2025-02-16 08:41:28 +0100

    added Datenschutz und Impressum text

--- src/pages/Impressum.tsx
+++ src/pages/Impressum.tsx
@@ -1,4 +1,3 @@
-
 import { Navbar } from "@/components/Navbar";
 
 const Impressum = () => {
@@ -12,7 +11,24 @@ const Impressum = () => {
           <div className="prose prose-lg">
             <h2>Angaben gemäß § 5 TMG</h2>
             <p>MaxCyclesCoaching</p>
-            {/* Add your Impressum content here */}
+            <h1>Impressum</h1>
+
+            <p>Maximilian Lohr<br />
+            MaxCyclesCoaching<br />
+            Heimst&auml;ttenweg 23<br />
+            01705 Freital</p>
+
+            <h2>Kontakt</h2>
+            <p>Telefon: +49 151 58424910<br />
+            E-Mail: maxcyclescoaching@gmail.com</p>
+
+            <h2>Gewerbeanmeldung</h2>
+            <p>Die Gewerbeerlaubnis nach &#91;Nummer des Paragraphen&#93; GewO wurde am 27.01.2025 von folgender Stelle erteilt: Stadtverwaltung Freital Gewerbeamt.</p>
+
+            <h2>Verbraucher&shy;streit&shy;beilegung/Universal&shy;schlichtungs&shy;stelle</h2>
+            <p>Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen.</p>
+
+            <p>Quelle: <a href="https://www.e-recht24.de/impressum-generator.html">https://www.e-recht24.de/impressum-generator.html</a></p>
           </div>
         </div>
       </main>
