c805385791772e60018e0c8459ea822f3ff6318e
Maximilian Lohr <s86327@stud.htw-dresden.de>
2025-03-03 13:27:34 +0100

    added strava link

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -279,9 +279,9 @@ const Index = () => {
             <h2 className="text-3xl md:text-4xl font-bold mb-8 text-primary">Das sagen meine Athlet*innen</h2>
             <div className="grid md:grid-cols-3 gap-8">
               <div className="bg-white p-8 rounded-lg shadow-md hover:shadow-xl transition-all duration-300">
-                <div className="flex flex-col items-center mb-4">
+                <div className="flex flex-col items-center mb-2">
                   <div className="w-16 h-16 bg-gray-200 rounded-full mb-4"></div>
-                  <div className="flex gap-1 mb-2">
+                  <div className="flex gap-1">
                     {[...Array(5)].map((_, i) => (
                       <svg key={i} className="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20">
                         <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
@@ -293,9 +293,9 @@ const Index = () => {
                 <p className="font-semibold text-primary">- Max Mustermann</p>
               </div>
               <div className="bg-white p-8 rounded-lg shadow-md hover:shadow-xl transition-all duration-300">
-                <div className="flex flex-col items-center mb-4">
+                <div className="flex flex-col items-center mb-2">
                   <div className="w-16 h-16 bg-gray-200 rounded-full mb-4"></div>
-                  <div className="flex gap-1 mb-2">
+                  <div className="flex gap-1">
                     {[...Array(5)].map((_, i) => (
                       <svg key={i} className="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20">
                         <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
@@ -307,13 +307,13 @@ const Index = () => {
                 <p className="font-semibold text-primary">- Anna Müller</p>
               </div>
               <div className="bg-white p-8 rounded-lg shadow-md hover:shadow-xl transition-all duration-300">
-                <div className="flex flex-col items-center mb-4">
+                <div className="flex flex-col items-center mb-2">
                   <img 
                     src="/images/jeremias_profile_img.jpg" 
                     alt="Profilbild von Jeremias Zieher" 
                     className="w-16 h-16 rounded-full mb-4"
                   />
-                  <div className="flex gap-1 mb-2">
+                  <div className="flex gap-1">
                     {[...Array(5)].map((_, i) => (
                       <svg key={i} className="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20">
                         <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
@@ -323,6 +323,7 @@ const Index = () => {
                 </div>
                 <p className="text-xl text-center mb-4">"Max lebt den Radsport, das merkt man. <br /> Er ist immer auf dem aktuellsten Stand und versucht, das Beste aus jedem Sportler durch gezieltes Training herauszuholen."</p>
                 <p className="font-semibold text-primary">- Jeremias Zieher, Dresden</p>
+                <a href="https://www.strava.com/athletes/23108452" target="_blank" className="text-primary hover:underline">Strava</a>
               </div>
             </div>
           </div>
