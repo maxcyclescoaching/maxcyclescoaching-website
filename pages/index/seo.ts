84624999a6d86dce5724bd3230d4c5c13cf40804
Maximilian Lohr <s86327@stud.htw-dresden.de>
2026-06-12 12:35:00 +0200

    Changed Main Page Title

--- pages/index/seo.ts
+++ pages/index/seo.ts
@@ -1,5 +1,5 @@
 export function getIndexSeo() {
-  const title = "Rennrad Coaching: Individuelles Training aus Dresden ab 99€/Monat";
+  const title = "Rennrad Coaching: Individuelle Trainingspläne ab 99€/Monat";
   const description =
     "Starte jetzt mit professionellem Rennrad Coaching - Individuelle Trainingspläne, persönliche Online-Betreuung und Motivation für deine Ziele.";
   const canonicalUrl = "https://maxcyclescoaching.de/";
